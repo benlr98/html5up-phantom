d462aeca00e941f74851af1e85eda63b94e808d5
ben.rich <benjamin.rich@ukg.com>
2023-01-23 12:54:59 -0500

    article data populates from API

--- assets/js/getArticleData.js
+++ assets/js/getArticleData.js
@@ -4,6 +4,10 @@
 (function() {
   let url = new URL(window.location.href);
   let idParam = url.searchParams.get("id");
+  let h1Element = document.querySelector('#main h1');
+  let imgElement = document.querySelector('#main img');
+  let pElement = document.querySelector('#main p');
+
 
   // get data for a specific instagram post 
   async function getArticleData() {
@@ -13,7 +17,29 @@
     return data;
   }
 
-  getArticleData();
+  async function replaceElementsWithData(){
+    let data = await getArticleData();
+
+    let regex = /#\w*/g;
+    // create post title from first #used in the post.caption
+    let hashtagArray = data.caption.match(regex);
+    let articleTitle = hashtagArray === null ? "#recipe" : hashtagArray[0];
+
+    let newTitleElement = document.createElement('h1');
+    let newImgElement = document.createElement('img');
+    let newPElement = document.createElement('p');
+
+    newTitleElement.innerHTML = articleTitle;
+    newImgElement.src = data.thumbnail_url;
+    newPElement.innerHTML = data.caption;
+
+
+    h1Element.replaceWith(newTitleElement);
+    imgElement.replaceWith(newImgElement);
+    pElement.replaceWith(newPElement);
+  };
+
+  replaceElementsWithData();
 
 
 
--- functions/getArticleData/getArticleData.js
+++ functions/getArticleData/getArticleData.js
@@ -2,14 +2,24 @@ const axios = require("axios");
 require("dotenv").config();
 
 const handler = async (event) => {
+  const postIdParam = event.queryStringParameters.id;
+  const accessToken = process.env.INSTA_TOKEN;
+  const fields =
+    "id,media_type,media_url,permalink,thumbnail_url,timestamp,username,caption";
+  const instaPostUrl = `https://graph.instagram.com/v15.0/${postIdParam}?fields=${fields}&access_token=${accessToken}`;
+  
   try {
-    const idParam = event.queryStringParameters.id || "World";
+    const { data } = await axios.get(instaPostUrl);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${idParam}` }),
+      body: JSON.stringify(data),
     };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+    const { message, code } = error;
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message, code }),
+    };
   }
 };
 
